19e6682977e9a294dea930b30b4f6d87d6bed024
Yariv Fishman <yarivfishman0@gmail.com>
2022-10-12 17:05:59 +0300

    Introduce custom debian 11 image builder

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Setup network interfaces.
+
+# Create users.
+
+# Change default password.
+
+# Set timezone and locale and keyboard.
+
+# Configure apt.
+
+# Install 3rd party packages.
+
+# Configure other things.
